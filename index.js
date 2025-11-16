aec392a8a31b5752e344c3325b0a886c82e995e4
jng7937 <155999181+jng7937@users.noreply.github.com>
2025-11-16 02:21:09 -0500

    changed ports in index.js

--- index.js
+++ index.js
@@ -21,12 +21,10 @@ const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "pages"));
 
-if (process.argv.length !== 3){
-    process.stdout.write("Usage index.js PORT_NUMBER");
-    process.exit(1);
-}
-const portNumber = process.argv[2];
-app.listen(portNumber); 
+const portNumber = process.env.PORT || 4000; 
+app.listen(portNumber, () => {
+  console.log(`App listening on port ${portNumber}`);
+});
 console.log(`Web server starting and running at http://localhost:${portNumber}`);
 app.use(bodyParser.urlencoded({extended:false}));
 
@@ -77,7 +75,7 @@ app.get("/country-data/:country", (request, response)=>{
         const collection = database.collection(collectionName);
         let countryData = await collection.findOne({country: countryName});
         if (!countryData){
-            console.log("ermmmmmmm theres no country like that");
+            console.log("theres no country like that");
             return res.status(404).json({ error: "Country not found" });
         } 
         response.json(countryData);
@@ -88,23 +86,5 @@ app.get("/country-data/:country", (request, response)=>{
         }
         
     })();
-
-});
-
-console.log("Stop to shut down the server: ");
-process.stdin.setEncoding("utf8");
-process.stdin.on('readable', () =>{
-    const dataInput = process.stdin.read();
-    if (dataInput !== null){
-        const command = dataInput.trim();
-    }
-    if (command === "stop"){
-        process.stdout.write("Shutting down the server");
-        process.exit(0);
-    }else{
-        process.stdout.write(`Invalid command: ${command}\n`);
-        console.log("Stop to shut down the server: ");
-    } 
-    process.stdin.resume();
 
 });
