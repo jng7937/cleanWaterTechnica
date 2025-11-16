8bdefe5ef7f894be9db7aaecd1651bc8ed8a0d10
jng7937 <jding755@gmail.com>
2025-11-16 00:06:10 -0500

    PIE CHART WORKS

---
+++ index.js
@@ -0,0 +1,102 @@
+const fs = require("fs");
+const express = require('express');
+const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+require("dotenv").config({
+   path: path.resolve(__dirname, "credentials/.env"),
+});
+app.use(express.static(path.join(__dirname, 'public')));
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+let limitedNum = 0;
+let basicNum = 0;
+let unimprovedNum = 0;
+let surfaceNum = 0;
+const databaseName = "technica_db";
+const collectionName = "water_data";
+const uri = process.env.MONGO_CONNECTION_STRING;
+const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "pages"));
+
+if (process.argv.length !== 3){
+    process.stdout.write("Usage index.js PORT_NUMBER");
+    process.exit(1);
+}
+const portNumber = process.argv[2];
+app.listen(portNumber); 
+console.log(`Web server starting and running at http://localhost:${portNumber}`);
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get("/", (request, response) => { 
+    let portLink = `http://localhost:${portNumber}/`;
+    console.log(portLink);
+    let variables = {
+        portNumberLink:portLink
+    };
+    response.render("index", variables);
+});
+
+app.get("/countries", (request, response) => {
+    (async () =>{
+        try{
+            await client.connect();
+            const database = client.db(databaseName);
+            const collection = database.collection(collectionName);
+            const countries = await collection.distinct("country");
+            //console.log(countries);
+            response.json(countries.sort());
+        }catch (e) {
+            console.error(e);
+            request.json([]);
+        } finally {
+            client.close();
+        }
+    }
+    )();
+});
+
+app.get("/country-data/:country", (request, response)=>{
+    const countryName = request.params.country;
+    console.log(`requested country: ${countryName}`);
+
+    (async ()=>{
+        try{
+            await client.connect();
+        const database = client.db(databaseName);
+        const collection = database.collection(collectionName);
+        let countryData = await collection.findOne({country: countryName});
+        if (!countryData){
+            console.log("ermmmmmmm theres no country like that");
+            return res.status(404).json({ error: "Country not found" });
+        } 
+        response.json(countryData);
+        }  catch (e) {
+            console.error(e);
+        } finally {
+            client.close();
+        }
+        
+    })();
+
+});
+
+console.log("Stop to shut down the server: ");
+process.stdin.setEncoding("utf8");
+process.stdin.on('readable', () =>{
+    const dataInput = process.stdin.read();
+    if (dataInput !== null){
+        const command = dataInput.trim();
+    }
+    if (command === "stop"){
+        process.stdout.write("Shutting down the server");
+        process.exit(0);
+    }else{
+        process.stdout.write(`Invalid command: ${command}\n`);
+        console.log("Stop to shut down the server: ");
+    } 
+    process.stdin.resume();
+
+});
---
+++ public/script.js
@@ -0,0 +1,96 @@
+console.log("server URL:", window.portLink);
+document.addEventListener("DOMContentLoaded", ()=> {loadCountries();})
+let pieChart= null;
+
+const selection = document.getElementById("selectCountry");
+selection.addEventListener("change", (country) => {
+    const selectedCountry = country.target.value;
+    console.log(`selected country in scriptjs: ${selectedCountry}`);
+    if (selectedCountry){
+        updateChart(selectedCountry);
+    }
+});
+
+
+async function loadCountries(){
+    console.log("loadCountries called");
+    const response = await fetch("/countries");
+    const countries = await response.json();
+    //console.log(`countriesList: ${countries}`);
+    selection.innerHTML = "";
+
+    countries.forEach(country => {
+        const option = document.createElement("option");
+        option.value = country;
+        option.textContent = country;
+        selection.appendChild(option);
+    });
+    console.log("Dropdown populated");
+}
+
+async function updateChart(country){
+    console.log("called update Chart");
+    const response = await fetch(`/country-data/${country}`);
+    if (!response.ok){
+        console.log("response was NOT ok");
+        return;
+    }
+    console.log("response was ok");
+    const data = await response.json();
+    console.log(`data from json: ${data}`);
+    const pieChartData = [
+        data.basic,
+        data.limited,
+        data.unimproved,
+        data.surfaceWater
+    ];
+    console.log(`piechart data: ${pieChartData}`);
+    console.log(pieChartData);
+
+    if (pieChart){
+        pieChart.destroy();
+    } 
+    var ctx = document.getElementById('myChart').getContext('2d');
+    pieChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['At least basic', 'Limited', 'Unimproved', 'Surface water'],
+            datasets: [{
+                label: 'Votes',
+                data: pieChartData,
+                backgroundColor: [
+                    'rgba(54, 162, 235, 0.6)',
+                    'rgba(255, 206, 86, 0.6)',
+                    'rgba(223, 135, 1, 0.6)',
+                    'rgba(237, 1, 1, 0.6)',
+                    
+                    
+                ],
+                borderColor: [
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(252, 101, 0, 1)',
+                    'rgba(255, 0, 0, 1)',
+                    
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                tooltip: {
+                    callbacks: {
+                        label: function(tooltipItem) {
+                            return tooltipItem.label + ': ' + tooltipItem.raw.toFixed(2);
+                        }
+                    }
+                }
+            }
+        }
+    });
+    
+}
